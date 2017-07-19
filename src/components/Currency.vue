6496a3e04915b5303a60ed74d32e463fa4aba9f7
fishmandev <fishman.code@gmail.com>
2017-07-19 15:22:02 +0300

    Make the currency rate editable

--- src/components/Currency.vue
+++ src/components/Currency.vue
@@ -6,14 +6,22 @@
                     <tr>
                         <th class="col-sm-1">#</th>
                         <th class="col-sm-2">Название</th>
-                        <th class="col-sm-2">Курс</th>
+                        <th class="col-sm-4">Курс</th>
                     </tr>
                 </thead>
                 <tbody v-if="currencies && currencies.length">
-                    <tr v-for="currency of currencies">
+                    <tr v-for="currency of currencies" :class="{ editing: currency == editCurrency }">
                         <th>{{ currency.symbol }}</th>
                         <td>{{ currency.name }}</td>
-                        <td>{{ currency.rate }}</td>
+                        <td @dblclick="editRate(currency)" class="rate-label">{{ currency.rate }}</td>
+                        <td class="rate-edit">
+                            <input type="text"
+                                   v-model="currency.rate"
+                                   v-currency-focus="currency == editCurrency"
+                                   @keyup.esc="cancelEdit(currency)"
+                                   @blur="doneEdit(currency)"
+                                   @keyup.enter="doneEdit(currency)">
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -28,7 +36,8 @@
     name: 'currency',
     data () {
       return {
-        currencies: []
+        currencies: [],
+        editCurrency: null
       }
     },
     created () {
@@ -39,12 +48,57 @@
         .catch(e => {
           console.log(e)
         })
+    },
+    methods: {
+      editRate (currency) {
+        this.beforeEditCache = currency.rate
+        this.editCurrency = currency
+      },
+      cancelEdit (currency) {
+        this.editCurrency = null
+        currency.rate = this.beforeEditCache
+      },
+      doneEdit (currency) {
+        // TODO Send the currency to backend
+        if (!this.editCurrency) {
+          return
+        }
+        this.editCurrency = null
+        currency.rate = currency.rate.trim()
+      }
+    },
+    // a custom directive to wait for the DOM to be updated
+    // before focusing on the input field.
+    // http://vuejs.org/guide/custom-directive.html
+    directives: {
+      'currency-focus': function (el, binding) {
+        if (binding.value) {
+          el.focus()
+        }
+      }
     }
   }
 </script>
 
 <style>
     .table {
-        width: initial;
+        width: 600px;
+    }
+
+    .editing .rate-label {
+        display: none;
+    }
+
+    .editing .rate-edit {
+        display: block;
+    }
+
+    .rate-edit {
+        display: none;
+    }
+
+    .rate-edit input {
+        padding: 0px;
+        border: none;
     }
 </style>
