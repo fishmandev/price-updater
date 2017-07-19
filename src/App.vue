f91c0d7c26834cba5fae64bf13d960982e1cb421
fishmandev <fishman.code@gmail.com>
2017-07-19 12:28:26 +0300

    Add the currency component and route

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,45 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <ul>
+            <li>
+                <router-link to="/">Home</router-link>
+            </li>
+            <li>|</li>
+            <li>
+                <router-link to="/currency">Currency</router-link>
+            </li>
+        </ul>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 10px;
+    }
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    li {
+        display: inline;
+    }
+
+    li > a {
+        text-decoration: none;
+    }
 </style>
---
+++ src/components/Currency.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="currency"></div>
+</template>
+
+<script>
+  export default {
+    name: 'currency',
+    data () {
+      return {
+        currencies: []
+      }
+    },
+    created () {
+      console.log('Loaded!')
+    }
+  }
+</script>
